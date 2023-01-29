9c126008baec9dc8c491b9b5d3620c635aa64198
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2023-01-29 11:49:58 -0500

    +orange to picker, refactored js

--- class12/background-picker/js/main.js
+++ class12/background-picker/js/main.js
@@ -1,19 +1,20 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
-
+document.getElementById('purple').onclick = partyPurple;
+document.getElementById('green').onclick = partyGreen;
+document.getElementById('blue').onclick = partyBlue;
+document.getElementById('orange').onclick = partyOrange;
 
 function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
-  document.querySelector('body').style.color = 'white'
+  document.body.style.backgroundColor = '#f13ff7';
 }
 
 function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'white'
+  document.body.style.backgroundColor = '#00fd51';
 }
 
 function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'white'
+  document.body.style.backgroundColor = '#00feff';
+} 
+
+function partyOrange() {
+  document.body.style.backgroundColor = '#ff8334';
 }
