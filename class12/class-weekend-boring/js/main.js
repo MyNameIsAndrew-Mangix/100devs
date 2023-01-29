32a654cad6010e30c1b4da4e2e68b846d08b2ba7
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2023-01-29 13:02:10 -0500

    refactor weekend-boring

--- class12/class-weekend-boring/js/main.js
+++ class12/class-weekend-boring/js/main.js
@@ -2,9 +2,16 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
+  const response = document.querySelector('#placeToSee');
 
-  //Conditionals go here
-
-
+  if (day === 'tuesday' || day === 'thursday') {
+    console.log("YOU HAVE CLASS");
+    response.textContent = "You Have Class";
+  } else if(day === "saturday" || day === "sunday") {
+    response.textContent = "It's the weekend"
+  }
+  else {
+    response.textContent = "BORING";
+  }
 }
